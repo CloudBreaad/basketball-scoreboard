c201e5bbcb06805cd26805457ffec2a84460dc3a
CloudBreaad <startingdima@gmail.com>
2025-11-12 13:39:55 +0700

    feat: add tournament image, reset button logic, and visual tweaks
    Inserted tournament.png between scoreboard panels to enhance visual context.
    Implemented JavaScript functionality for the reset button to clear scores dynamically.
    Adjusted reset text color for better visibility and contrast against dark background.
    Refined layout alignment to maintain precision across screen sizes.

---
+++ index.js
@@ -0,0 +1,41 @@
+let basics = document.getElementById("home");
+let tamu = document.getElementById("guest");
+
+let basic= 0
+let tamus = 0
+
+function one(){
+    tamus += 1
+   tamu.textContent=tamus
+}
+function two(){
+    tamus += 2
+    tamu.textContent=tamus
+}
+function three(){
+    tamus += 3
+    tamu.textContent=tamus
+}
+function resetlawan(){
+    tamus = 0
+    tamu.textContent='00'
+}
+//police-line. do not cross     police-line. do not cross 
+function plusone(){
+    basic +=1
+    basics.textContent=basic
+}
+function plustwo(){
+    basic +=2
+    basics.textContent=basic
+}
+function plusthree(){
+    basic +=3
+    basics.textContent=basic
+}
+
+function resetkawan(){
+    basic = 0
+    basics.textContent='00'
+}
+
